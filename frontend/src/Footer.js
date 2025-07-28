b2b8c9658e0e97ddfc4dbd7335f84f6e39c08596
Prashant4050 <malaviyaprashant4050@gmail.com>
2025-07-28 21:32:16 +0530

    version 2

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Footer.css';
-import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -11,24 +10,17 @@ const Footer = () => {
           <p>Prashant Malaviya</p>
           <p>Het Patel</p>
         </div>
-
         <div className="footer-section">
           <h4>Email Ids</h4>
-          <p>malaviyaprashant4050@gmail.com</p>
-          <p>hetpatell0305@gmail.com</p>
+          <p> malaviyaprashant4050@gmail.com</p>
+          <p> hetpatell0305@gmail.com</p>
         </div>
-
         <div className="footer-section">
-          <h4>Follow Us</h4>
-          <a href="https://github.com/Prashant4050/practice" target="_blank" rel="noreferrer">
-            <FaGithub className="icon" /> Prashant4050
-          </a>
-          <a href="https://github.com/hetpatel0305/practice" target="_blank" rel="noreferrer">
-            <FaGithub className="icon" /> hetpatel0305
-          </a>
+          <h4>Follow Us </h4>
+          <a href="https://github.com/Prashant4050/practice" target="_blank" rel="noreferrer" >Prashant4050</a>
+          <a href="https://github.com/het0305/practice" target="_blank" rel="noreferrer">hetpatel0305</a>
         </div>
       </div>
-
       <div className="footer-bottom">
         <p>&copy; 2025 Flipy. All rights reserved.</p>
       </div>
